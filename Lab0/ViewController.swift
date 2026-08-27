65528e36e06e9bec86d7771b55846bf1f91de8f3
Brady Boedy <bradyboedy@ku.edu>
2026-08-27 12:40:32 -0400

    Added Optional Requirements

--- Lab0/ViewController.swift
+++ Lab0/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var majorNameTextField: UITextField!
     
     @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
     
@@ -35,8 +36,9 @@ class ViewController: UIViewController {
 
             // Creating a variable of type string, that holds an introduction. The introduction interpolates the values from the text fields provided.
             // Currently we can only present the information in a print statement. However, this lets us verify that our app is printing out what is intended!
-            let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I am studying \(majorNameTextField.text!). I own \(numberOfPetsLabel.text!) pet(s). It is \(morePetsSwitch.isOn) that I want more pets."
 
+        
             // Creates the alert where we pass in our message, which our introduction.
             let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
 
@@ -49,9 +51,39 @@ class ViewController: UIViewController {
             present(alertController, animated: true, completion: nil)
         }
     
+    
+    //changes the background to red
+    @IBAction func changeBackground(_ sender: UIButton) {
+        view.backgroundColor = .red
+    }
+    
+    //changes the background back to white
+    
+    @IBAction func revertBackground(_ sender: UIButton) {
+        view.backgroundColor = .white
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //I changed the text inputs to be rounded instead of square
+        //I had to thicken the border of the textbox too because when I rounded the edges you could barely see it
+        firstNameTextField.layer.cornerRadius = 15
+        firstNameTextField.layer.masksToBounds = true
+        firstNameTextField.layer.borderWidth = 2
+
+        lastNameTextField.layer.cornerRadius = 15
+        lastNameTextField.layer.masksToBounds = true
+        lastNameTextField.layer.borderWidth = 2
+
+        schoolNameTextField.layer.cornerRadius = 15
+        schoolNameTextField.layer.masksToBounds = true
+        schoolNameTextField.layer.borderWidth = 2
+        
+        majorNameTextField.layer.cornerRadius = 15
+        majorNameTextField.layer.masksToBounds = true
+        majorNameTextField.layer.borderWidth = 2
     }
 
 
--- README.md
+++ README.md
@@ -25,6 +25,6 @@ The introduction app allows users to enter their first name, last name, and the 
 
 ### Optional Features
 
-- [ ] 1. User can tap a button to change the color of the background view
-- [ ] 3. User can select on additional buttons that provide more info about the user. Example: more textfields, a different alert box, etc.
-- [ ] 4. Any stylistic changes that are not default options (Comment this here)
+- [x] 1. User can tap a button to change the color of the background view (User can now change it to red, and then back to white)
+- [x] 3. User can select on additional buttons that provide more info about the user. (User can now add their major into the app)
+- [x] 4. Any stylistic changes that are not default options (I rounded the borders of the textboxes instead of their original rectangular form)
